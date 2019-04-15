f382a21ff46b645c5e5ee5b0eb3ae305fb509744
Roy <Nilanjan.Roy@emc.com>
2019-04-15 15:05:31 +0530

    initial commit

---
+++ src/main/java/com/nilanjan/oauth/SpringBootOauthUaaApplication.java
@@ -0,0 +1,13 @@
+package com.nilanjan.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthUaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauthUaaApplication.class, args);
+    }
+
+}
